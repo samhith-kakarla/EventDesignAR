366bdac61bd78bf7eb2e574596fc8857aa2f1adb
Samhith Kakarla <samhith.kakarla@gmail.com>
2021-08-29 17:47:55 -0400

    some flutter setup

---
+++ frontend/lib/screens/User/Landing.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class LandingScreen extends StatefulWidget {
+  const LandingScreen({ Key? key }) : super(key: key);
+
+  @override
+  _LandingScreenState createState() => _LandingScreenState();
+}
+
+class _LandingScreenState extends State<LandingScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
