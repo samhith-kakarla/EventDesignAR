ca28a6f82502ee09c072926bef71df2576482044
Samhith Kakarla <samhith.kakarla@gmail.com>
2021-10-21 21:56:59 -0400

    refactored redux

--- frontend/src/redux/actions/userActions.js
+++ frontend/src/redux/actions/userActions.js
@@ -1,20 +1,10 @@
 // TYPES
-export const SET_IS_AUTHENTICATED = 'SET_IS_AUTHENTICATED';
-export const SET_SPOTIFY_PROFILE_URL = 'SET_SPOTIFY_PROFILE_URL';
 export const BACKEND_FETCHED = 'SET_BACKEND_FETCHED';
 export const BACKEND_ERROR = 'BACKEND_ERROR';
 export const SET_USER = 'SET_USER';
 export const UNSET_USER = 'UNSET_USER';
 
 // ACTIONS
-export function setIsAuthenticated() {
-  return { type: SET_IS_AUTHENTICATED, payload: {} };
-}
-
-export function setSpotifyProfileURL(spotifyProfileURL) {
-  return { type: SET_SPOTIFY_PROFILE_URL, payload: { spotifyProfileURL } };
-}
-
 export function backendFetched() {
   return { type: BACKEND_FETCHED, payload: {} };
 }
--- frontend/src/redux/reducers/user.js
+++ frontend/src/redux/reducers/user.js
@@ -2,24 +2,12 @@ import * as types from '../actions/userActions';
 
 const initialState = {
   user: null,
-  spotifyProfileURL: '',
-  isAuthenticated: false,
   backendFetched: false,
   backendError: false,
 };
 
 const userReducer = (state = initialState, action) => {
   switch (action.type) {
-    case types.SET_IS_AUTHENTICATED:
-      return {
-        ...state,
-        isAuthenticated: true,
-      };
-    case types.SET_SPOTIFY_PROFILE_URL:
-      return {
-        ...state,
-        spotifyProfileURL: action.payload.spotifyProfileURL,
-      };
     case types.BACKEND_FETCHED:
       return {
         ...state,
